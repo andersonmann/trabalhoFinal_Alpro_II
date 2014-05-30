5b1816d2a0d37ce30113b659788df53ef55ec6f2
anderson mann <anderson.civil@hotmail.com>
2014-05-29 22:01:46 -0300

    commit supermercado v1

---
+++ TF/src/supermercado_mann/EmptyQueueException.java
@@ -0,0 +1,14 @@
+package supermercado_mann;
+
+public class EmptyQueueException extends RuntimeException
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyQueueException()
+	{
+	    super();
+	}
+}
---
+++ TF/src/supermercado_mann/QueueTAD.java
@@ -0,0 +1,11 @@
+package supermercado_mann;
+
+public interface QueueTAD<E>
+{
+    void add(E element);
+    E remove();
+    int size();
+    boolean isEmpty();
+    void clear();
+    E element();
+}
